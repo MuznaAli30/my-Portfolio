1c6e687884fab190662336c818a1410a69da48f7
MuznaAli30 <muznaalisiddiquisabzwari@gmail.com>
2024-08-10 13:35:34 +0500

    Updated projects section and made images uniform size

--- src/Home/Resume/Resume.js
+++ src/Home/Resume/Resume.js
@@ -3,8 +3,8 @@ import MuznaResume from '../../Assets/MuznaResume.jpg';
 
 function ResumeNew() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#cdcbd6]">
-      <img className="mb-5 max-w-full h-auto" src={MuznaResume} alt="Resume" />
+    <div className="flex justify-center items-center min-h-screen bg-[#cdcbd6] ">
+      <img className="m-10 max-w-[90%] h-auto shaddow-2xl rounded-2xl max-sm:w-[90%] max-lg:w-[90%] max-xl:w-[90%]" src={MuznaResume} alt="Resume" />
     </div>
   );
 }
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
-// Import project images
 import cfe from '../Assets/cfe.webp';
 import imgTK from '../Assets/imgTK.webp';
 import cmps from '../Assets/cmps.webp';
-import prkng from '../Assets/prkng.webp';
 import  todo  from '../Assets/todo.webp';
-// import About from '../Home/About/About';
 import { Link } from 'react-router-dom';
+import parking1 from '../Assets/parking1.webp';
 
 export default function Projects() {
   // Array of project objects
@@ -37,10 +34,10 @@ export default function Projects() {
       link: 'https://www.loom.com/embed/ec6fd2df58a44628a241bbbe5d5782d3?sid=99aec77c-043d-4cec-a320-0debff54be71'
     },
     {
-      image: prkng,
+      image: parking1,
       title: 'A Real Time Parking Website ',
-      description: 'Get ready for the launch of a real-time CRUD (Create, Read, Update, Delete) website, currently being developed on the MERN stack (MongoDB, Express.js, React.js, Node.js), with HTML, Tailwind CSS, and TypeScript. Stay tuned for an innovative platform that will empower users to book parking spaces in real-time, offering efficient CRUD operations for managing parking availability.',
-      link: ''
+      description: 'Explore a real-time CRUD (Create, Read, Update, Delete) website built on the MERN stack (MongoDB, Express.js, React.js, Node.js), with HTML, Tailwind CSS, and TypeScript. This innovative platform allows users to book parking spaces in real-time, offering efficient CRUD operations for managing parking availability.',
+      link: 'https://www.loom.com/embed/e8f18d471c7e4f869f9388d10921932b?sid=444c6b25-ed3c-4fd7-a252-c193a30721dd'
     }
   ];
 
@@ -61,8 +58,8 @@ export default function Projects() {
           <div className="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
             {/* Map through projects array */}
             {projects.map((project, index) => (
-              <div key={index} className="space-y-4">
-                <img src={project.image} alt={project.title} className="w-full h-auto rounded-xl" />
+              <div key={index} className="m-auto space-y-7">
+                <img src={project.image} alt={project.title} className="shaddow-2xl w-full h-[40%] rounded-xl" />
                 <h3 className="text-2xl font-bold leading-tight text-[#d96846]">
                   {project.title}
                 </h3>
@@ -72,22 +69,14 @@ export default function Projects() {
                 <Link to={project.link} title=""
                   className="text-xl text-[#2f3020] bg-primary-700 justify-center hover:bg-[#d96846] inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   role="button">
-                  View Project
-                  <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path fillRule="evenodd"
-                      d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                      clipRule="evenodd" />
-                  </svg>
+                  View Project<i class="fa-solid fa-arrow-right"></i>
                 </Link>
               </div>
             ))}
           </div>
         </div>
-      {/* <About/> */}
 
       </section>
-      {/* <Footer /> */}
     </>
   );
 }
